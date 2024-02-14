6037565c2de97dbc10bed6d98fa6465f2abd5e44
wettestsock <evilsharkeater@gmail.com>
2024-02-14 12:54:06 -0500

    nothing of note

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ r.attributes('-type', 'dialog')
 r.geometry(f'{window_w}x{window_h}') 
 
 
-termf = Frame(r, height=window_w-100, width=window_h)
+termf = Frame(r, height=window_w, width=window_h)
 
-termf.pack(fill=BOTH, expand=YES)
+termf.pack(expand=YES)
 wid = termf.winfo_id()
 os.system(f'xterm -into %d -fg white -bg black -geometry {window_w}x{window_h} -sb &' % wid)
 
