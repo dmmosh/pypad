c69960cb9d023a21dfa3a58a51043b914ce2d109
wettestsock <evilsharkeater@gmail.com>
2024-02-14 12:52:45 -0500

    nothing of note

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 
 from tkinter import * 
-from tkinter import messagebox as ms
-from tkinter.simpledialog import askstring
+from tkinter import colorchooser as cc
+
 import os
 
+# ROOT WINDOW
+r = Tk() 
 
+def quit():
+    r.destroy()
+
+def color():
+    return cc.askcolor(title="background color:")
 
 
   
-# ROOT WINDOW
-r = Tk() 
 
 window_w = r.winfo_screenwidth()//4
 window_h = r.winfo_screenheight()//10
@@ -17,14 +22,12 @@ window_h = r.winfo_screenheight()//10
 r.attributes('-type', 'dialog')
 r.geometry(f'{window_w}x{window_h}') 
 
-def quit():
-    r.destroy()
 
-termf = Frame(r, height=window_w, width=window_h, bg='black')
+termf = Frame(r, height=window_w, width=window_h-100, bg='black')
 
 termf.pack(fill=BOTH, expand=YES)
 wid = termf.winfo_id()
-os.system(f'xterm -into %d -fg white -bg black -geometry {window_w}x{window_h} -sb &' % wid)
+os.system(f'xterm -into %d -fg white -bg -geometry {window_w}x{window_h} -sb &' % wid)
 
 
 button = Button(r, text="quit", command=quit)
