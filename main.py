bfdf378a388e75874521e8abba89993ce59ce271
wettestsock <evilsharkeater@gmail.com>
2024-02-14 12:40:34 -0500

    nothing of note

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ termf = Frame(r, height=window_w, width=window_h)
 
 termf.pack(fill=BOTH, expand=YES)
 wid = termf.winfo_id()
-os.system('xterm -into %d -geometry {}x{} -sb &'.format(window_w,window_h) % wid)
+os.system('xterm -into %d -geometry {}x{} -bg black -fg white -sb &'.format(window_w,window_h) % wid)
+
 
 button = Button(r, text="quit", command=quit)
 button.pack()
