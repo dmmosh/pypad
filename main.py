9b104ebb0825d9c1ee669986dd3083b76fc04408
wettestsock <evilsharkeater@gmail.com>
2024-02-14 12:42:09 -0500

    nothing of note

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ termf = Frame(r, height=window_w, width=window_h)
 
 termf.pack(fill=BOTH, expand=YES)
 wid = termf.winfo_id()
-os.system("xterm -into %d -fn -misc-fixed-medium-r-normal--8-80-75-75-c-50-iso10646-1 -geometry {}x{}+0+0 +sb -bg black -fg white -e /root/.bashrc &".format(window_w,window_h) % id);  
+os.system("xterm -into %d -fn -misc-fixed-medium-r-normal--8-80-75-75-c-50-iso10646-1 -geometry {}x{}+0+0 +sb -bg black -fg white -e /root/.bashrc &".format(window_w,window_h) % wid);  
 
 
 button = Button(r, text="quit", command=quit)
