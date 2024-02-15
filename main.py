92a60b6ee93ccb503e5b41ef02846a19c602d9cc
wettestsock <evilsharkeater@gmail.com>
2024-02-15 00:52:53 -0500

    nothing of note

--- main.py
+++ main.py
@@ -59,9 +59,9 @@ btn = {
 
 r.attributes('-type', 'dialog')
 r.geometry(f"{ var['win_w'] }x{ var['win_h'] }+{ var['loc_x'] }+{ var['loc_y'] }") 
-r.bind()
 
-r.bind("<Leave>", r.destroy)
+if var['hover_quit']:
+    r.bind("<Leave>", quit)
 
 term = Frame(r, height=var['win_h'], width=var['win_w']-50)
 #term = Text(r, height=var['win_h'], width=var['win_w']-50)
