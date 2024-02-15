c9eb6ed1a654b7d81895f11f14eb106642e0452f
wettestsock <evilsharkeater@gmail.com>
2024-02-14 23:15:27 -0500

    nothing of note

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ var = {
     'color_bg': 'black',
     'color_fg': 'white',
     'loc_x': width - 100,
-    'loc_y': height - 100
+    'loc_y': height
 }
 #var['loc_x'] = (r.winfo_screenwidth()//2) - (var['win_w']//2)
 #var['loc_y'] = (r.winfo_screenheight()//2) - (var['win_h']//2)
