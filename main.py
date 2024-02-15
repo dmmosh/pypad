177eae357359f5629ae84791431c0f2fe40764b4
wettestsock <evilsharkeater@gmail.com>
2024-02-15 01:01:45 -0500

    nothing of note

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ var = {
 btn = {
     'quit': Button(r, text='quit', command=quit),
     'color': Button(r, text='color', command=color),
-    'settings': Button(r, image=PhotoImage(file='./settings.png'), command=quit)
+    'settings': Button(r, image=PhotoImage(file='./settings.png', width=20, height=20), command=quit)
 }
 #var['loc_x'] = (r.winfo_screenwidth()//2) - (var['win_w']//2)
 #var['loc_y'] = (r.winfo_screenheight()//2) - (var['win_h']//2)
