a9477d1c87ada53b7682a7a67fbac6515ce67e35
wettestsock <evilsharkeater@gmail.com>
2024-02-15 00:47:35 -0500

    nothing of note

--- main.py
+++ main.py
@@ -37,23 +37,20 @@ width, height = r.winfo_screenwidth(), r.winfo_screenheight()
 
 
 
+
 var = {
     'win_w': width//6, # window width
     'win_h': height//10, #window height
     'color_bg': 'black', #default color for background
     'color_fg': 'white', # default color for text
     'loc_x': width-(int(width/5.7)), # x coordinate for the terminal window
-    'loc_y': height -(int(height/6)), # y coordinate for the terminal window
-
-    'cursor_quit': False # if want to quit when cursor leaves the calculator
+    'loc_y': height -(int(height/6)) # y coordinate for the terminal window
 }
-
     
 btn = {
     'quit': Button(r, text='quit', command=quit),
     'color': Button(r, text='color', command=color),
-    'settings': Button(r, image=PhotoImage(file='./settings.png'), command=quit),
-    'exit': Button(r, height=var['win_h'], width=var['win_w'], command=quit, bg='red')
+    'settings': Button(r, image=PhotoImage(file='./settings.png'), command=quit)
 }
 #var['loc_x'] = (r.winfo_screenwidth()//2) - (var['win_w']//2)
 #var['loc_y'] = (r.winfo_screenheight()//2) - (var['win_h']//2)
@@ -62,22 +59,15 @@ r.attributes('-type', 'dialog')
 r.geometry(f"{ var['win_w'] }x{ var['win_h'] }+{ var['loc_x'] }+{ var['loc_y'] }") 
 
 
-term = Text(r, height=var['win_h'], width=var['win_w']-50)
+term = Frame(r, height=var['win_h'], width=var['win_w']-50)
 #term = Text(r, height=var['win_h'], width=var['win_w']-50)
 term.pack(side=LEFT, anchor=NW)
 wid = term.winfo_id()
 
-#term.bind("<Enter>", exit)
-
-if(var['cursor_quit']): # if cursor quit is enabled
-    term.bind("<Leave>", exit)
-
 os.system(f"xterm -fa 'Monospace' -fs 17 -rightbar -into {wid} -geometry {var['win_h']}x{var['win_w']-50} -bg {var['color_bg']} -fg {var['color_fg']} -sb -e 'clear && /usr/bin/python -q && exit' &")
 
 
-
-
-
+    
 #settings = Button(r, image=PhotoImage(file='./settings.png'), command=quit)
 #settings.pack()
 #set_img = PhotoImage(file='settings.png')
