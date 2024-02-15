eab3e32a3838ff13a89b3675671627f650dffce4
wettestsock <evilsharkeater@gmail.com>
2024-02-15 00:27:11 -0500

    nothing of note

--- main.py
+++ main.py
@@ -65,9 +65,8 @@ term.pack(side=LEFT, anchor=NW)
 wid = term.winfo_id()
 
 # TODO: in final build, change where the yes / no message gets sent to (.config folder)
-os.system(f"xterm -fa 'Monospace' -fs 17 -rightbar -into {wid} -geometry {var['win_h']}x{var['win_w']-50} -bg {var['color_bg']} -fg {var['color_fg']} -sb -e 'clear && /usr/bin/python -q' &")
+os.system(f"xterm -fa 'Monospace' -fs 17 -rightbar -into {wid} -geometry {var['win_h']}x{var['win_w']-50} -bg {var['color_bg']} -fg {var['color_fg']} -sb -e 'clear && /usr/bin/python -q && pwd' &")
 
-print(dir)
 
 
 #settings = Button(r, image=PhotoImage(file='./settings.png'), command=quit)
