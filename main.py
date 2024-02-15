c62464514936fe9f6cb4eaa863032a635788d328
wettestsock <evilsharkeater@gmail.com>
2024-02-15 00:31:24 -0500

    nothing of note

--- main.py
+++ main.py
@@ -66,8 +66,7 @@ wid = term.winfo_id()
 
 os.system(f"xterm -fa 'Monospace' -fs 17 -rightbar -into {wid} -geometry {var['win_h']}x{var['win_w']-50} -bg {var['color_bg']} -fg {var['color_fg']} -sb -e 'clear && /usr/bin/python -q && exit' &")
 
-if not (term.winfo_ismapped()):
-    print("DOESNT EXIST")
+term.destroy()
 
 #settings = Button(r, image=PhotoImage(file='./settings.png'), command=quit)
 #settings.pack()
