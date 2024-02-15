7c835c68f387397990fead9ae1dfe8620f05bba4
wettestsock <evilsharkeater@gmail.com>
2024-02-15 00:56:47 -0500

    nothing of note

--- main.py
+++ main.py
@@ -65,7 +65,6 @@ term = Frame(r, height=var['win_h'], width=var['win_w']-50)
 #term = Text(r, height=var['win_h'], width=var['win_w']-50)
 term.pack(side=LEFT, anchor=NW)
 wid = term.winfo_id()
-term.bind("<Leave>", func=quit)
 
 os.system(f"xterm -fa 'Monospace' -fs 17 -rightbar -into {wid} -geometry {var['win_h']}x{var['win_w']-50} -bg {var['color_bg']} -fg {var['color_fg']} -sb -e 'clear && /usr/bin/python -q && exit' &")
 
