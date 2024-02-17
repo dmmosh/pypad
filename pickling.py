5528bc426f086d1e40a7b2ad5916a00a13b1266a
wettestsock <evilsharkeater@gmail.com>
2024-02-16 22:22:44 -0500

    nothing of note

--- pickling.py
+++ pickling.py
@@ -6,5 +6,7 @@ colors = pd.read_excel("data.xlsx").to_dict('list')
 
 object = pickle.dump(colors, open('data.obj', 'wb'))
 
+object = pickle.load(open('data.obj', 'wb'))
+
 colors = None
 print(object)
