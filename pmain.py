cae63bd3f4813831b8960749b39b2646070e850c
wettestsock <evilsharkeater@gmail.com>
2024-02-18 18:44:02 -0500

    nothing of note

--- pmain.py
+++ pmain.py
@@ -93,9 +93,6 @@ wid = term.winfo_id()
 # puts process's pid as arg 1 (will be deleting later)
 os.system(f"xterm -fa \'{gl.var['font']}\' -fs {gl.var['font_size']} -rightbar -into {wid} -geometry {gl.var['win_h']}x{gl.var['win_w']-50} -bg {gl.var['color_bg']} -fg {gl.var['color_fg']} -sb -e 'clear && /usr/bin/python -q -i {gl.dir_loc}/exec.py {str(os.getpid())} && exit' &")
 
-while(1):
-    print(term.get())
-    time.sleep(2)
 
 
 if gl.var['auto_cursor'] == True:
