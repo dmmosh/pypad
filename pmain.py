f08acb7375d3e74c734b871101724dda8b64d367
wettestsock <evilsharkeater@gmail.com>
2024-02-18 14:24:43 -0500

    nothing of note

--- pmain.py
+++ pmain.py
@@ -257,6 +257,7 @@ class settings:
     def default(self):
         temp = pickle.load(open(dir+'/default_var.obj', 'rb'))
         print(temp)
+        var['color_bg'] = 'Black'
         var['global_font'] = None # cant pickle tkinter objects
         dump_var()
 
