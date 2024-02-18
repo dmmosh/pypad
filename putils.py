9ca5214b697b5807f62b5f8d72971dca3c472175
wettestsock <evilsharkeater@gmail.com>
2024-02-18 16:59:14 -0500

    nothing of note

--- putils.py
+++ putils.py
@@ -213,7 +213,7 @@ class settings:
                                 font=gl.var['global_font'],
                                 background=gl.var['color_fg'],
                                 foreground=gl.var['color_bg'])
-        self.drop_bg.current(self.data['name'].index(gl.var['color_bg']))
+        self.drop_bg.current(gl.var['color_bg'])
         self.drop_bg.pack(anchor=W, padx= 7, pady= 7)
 
         text(self.theme, "Foreground color:").pack(anchor=W, padx= 7, pady= 3)
@@ -222,7 +222,7 @@ class settings:
                                 font=gl.var['global_font'],
                                 background=gl.var['color_fg'],
                                 foreground=gl.var['color_bg'])
-        self.drop_fg.current(self.data['name'].index(gl.var['color_fg']))
+        self.drop_fg.current(gl.var['color_fg'])
         self.drop_fg.pack(anchor=W, padx= 7, pady= 7)
 
 
