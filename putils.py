170f4521cbce51b28b0a6167026df37ce7ce699b
wettestsock <evilsharkeater@gmail.com>
2024-02-18 15:15:04 -0500

    nothing of note

--- putils.py
+++ putils.py
@@ -82,7 +82,7 @@ def msg_box(message:str = "Error", title:str = 'ERROR', width:int = 300, height:
 # yes or no prompt
 def yes_or_no(window = gl.r, message:str = "Yes or no?", width:int = 300, height:int = 200) -> int:
     box = Toplevel(window, background=gl.var['color_bg'])
-    box.geometry(f'{gl.width}x{gl.height}')
+    box.geometry(f'{width}x{height}')
     box.attributes('-type', 'dialog') # makes it a floating window
     box.bind('<Escape>', lambda event:quit(box))
     box.title('Yes or no?')
