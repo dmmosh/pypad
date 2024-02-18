87178b0e8552e8984dba2c001d31339a53ef4f07
wettestsock <evilsharkeater@gmail.com>
2024-02-18 18:04:23 -0500

    nothing of note

--- pypad/exec.py
+++ pypad/exec.py
@@ -9,12 +9,12 @@ libraries = []
 # QUIT AND EXIT FUNCTION OVERRIDE
 #TODO: change what this closes
 def quit():
-    os.system("pkill -9 -f " + sys.argv[1]) #kills the pypad executable
+    os.system("kill -9 -f " + sys.argv[1]) #kills the pypad executable
     
     sys.exit()
 
 def exit():
-    os.system("pkill -9 -f " + sys.argv[1]) #kills the pypad executable
+    os.system("kill -9 -f " + sys.argv[1]) #kills the pypad executable
     sys.exit()
 
 print(sys.argv[1])
