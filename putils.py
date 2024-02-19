f6e1337cd3373fdabccabfeab8f911214c2a8bfc
wettestsock <evilsharkeater@gmail.com>
2024-02-18 19:13:31 -0500

    nothing of note

--- putils.py
+++ putils.py
@@ -278,7 +278,7 @@ class settings:
             msg_box('Cannot save due to lacking permissions.\nTry running \"sudo chown $USER /usr/share/pypad/\"', width=700, height=150)
         else:
             if yes_or_no(message='Settings saved.\nRestart now?', width= 300) == 1:
-                os.execl(sys.executable, sys.executable, 'restart')
+                os.execl(sys.executable, sys.executable, [sys.argv[0], 'restart'])
             
 
 
@@ -291,7 +291,7 @@ class settings:
         else:
             
             if yes_or_no(message='Settings defaulted.\nRestart now?', width= 400) == 1:
-                os.execl(sys.executable, sys.executable, 'restart')
+                os.execl(sys.executable, sys.executable, [sys.argv[0], 'restart'])
 
     # checks if input is an integer
     def callback(self, P):
