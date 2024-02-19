e424cbdecff2c3848d6f26f8d1c218bee85e1927
wettestsock <evilsharkeater@gmail.com>
2024-02-18 21:18:39 -0500

    nothing of note

--- pypad/exec.py
+++ pypad/exec.py
@@ -1,37 +1,28 @@
 import sys
+from pynput.keyboard import Key, Listener, KeyCode
 import os
+from math import * 
 #from numpy import *
-
-from math import *
 
 
 # QUIT AND EXIT FUNCTION OVERRIDE
 #TODO: change what this closes
 def quit():
-    os.system("kill -9 -f " + sys.argv[1]) #kills the script (if running)
     os.system("pkill -9 -f pypad") #kills the pypad executable
+
     sys.exit()
 
 def exit():
-    os.system("kill -9 -f " + sys.argv[1])
     os.system("pkill -9 -f pypad") #kills the pypad executable
     sys.exit()
 
-# MAKES SURE THAT PYNPUT IS INSTALLED 
-# if not, can't execute fast num lock exit
-try:
-    from pynput.keyboard import Key, Listener, KeyCode
-except:
-    print("Pynput not found. Quick Num lock escape not possible. Consider installing pynput by typing \"pip install pynput\"")
-else:
-    def on_press(key):
-        if key == Key.num_lock:
-            exit()
-    # keeps the listener on
-    listener = Listener( on_press=on_press)
+def on_press(key):
+    if key == Key.num_lock:
+        exit()
+    
 
-    # starts the listener
-    listener.start()
+# keeps the listener on
+listener = Listener( on_press=on_press)
 
-
-
+# starts the listener
+listener.start()
