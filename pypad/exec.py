4aaab350b6d31f45a12f6b2d4be13497521c4bdd
wettestsock <evilsharkeater@gmail.com>
2024-02-28 14:35:53 -0500

    nothing of note

--- pypad/exec.py
+++ pypad/exec.py
@@ -21,8 +21,6 @@ def on_press(key):
     if key == Key.num_lock:
         exit()
     
-f = open(os.environ['HOME']+"/.pypad", 'w')
-sys.stdout = f
 try:
     from pynput.keyboard import Key, Listener, KeyCode
 
