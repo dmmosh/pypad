ebcda8d19a6703eaf8955d7b25f60aa8b8c560bf
wettestsock <evilsharkeater@gmail.com>
2024-03-01 15:35:45 -0500

    nothing of note

--- pypad/exec.py
+++ pypad/exec.py
@@ -21,17 +21,32 @@ def key_press(key):
     if key == k.Key.num_lock:
         exit()
 
-def backspace():
-    print("BACKSPACE ON")
+def on_activate():
+    '''Defines what happens on press of the hotkey'''
+    k.type('Kind regards, John Smith, Sales Manager.')
 
+def for_canonical(hotkey):
+    '''Removes any modifier state from the key events 
+    and normalises modifiers with more than one physical button'''
+    return lambda k: hotkey(k.Listener.canonical(k))
+
+'''Creating the hotkey'''
+hotkey = k.HotKey(
+k.HotKey.parse('<shift>+k'), 
+on_activate)
+
+    
 try:
     from pynput import keyboard as k
 
     # keeps the listener on
     k.Listener( on_press=key_press).start()
-    k.GlobalHotKeys({
-        'g+h': backspace
-    }).start()
+    
+    with k.Listener(
+            on_press=for_canonical(hotkey.press),
+            on_release=for_canonical(hotkey.release)) as listener:
+        listener.join()
+
 
 except:
     print("PYNPUT NOT FOUND. Quick escape not set. Consider typing \"os.system(\"pip install pynput\")\"")
