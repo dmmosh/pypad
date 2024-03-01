3f22755b7f030545af51008674c6333d94e65b28
wettestsock <evilsharkeater@gmail.com>
2024-03-01 17:00:31 -0500

    nothing of note

--- pypad/exec.py
+++ pypad/exec.py
@@ -9,16 +9,16 @@ from math import *
 #TODO: change what this closes
 def quit():
     os.system("pkill -9 -f pypad") #kills the pypad executable
+    os.system("pkill -9 -f pmain.py") #kills the pypad script (comment out after debugging)
     
     sys.exit()
 
 def exit():
-    os.system("pkill -9 -f pypad") #kills the pypad executable
-    sys.exit()
+    quit()
 
 def on_press(key):
     if key == Key.num_lock:
-        exit()
+        quit()
     
 try:
     from pynput.keyboard import Key, Listener
