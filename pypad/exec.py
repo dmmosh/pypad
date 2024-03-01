f5d145793bbf1e2fc909dcc5d76610789745569a
wettestsock <evilsharkeater@gmail.com>
2024-03-01 16:28:41 -0500

    nothing of note

--- pypad/exec.py
+++ pypad/exec.py
@@ -16,8 +16,21 @@ def exit():
     os.system("pkill -9 -f pypad") #kills the pypad executable
     sys.exit()
 
-
+# key press 
+def key_press(key):
+    if key == Key.num_lock:
+        exit()
 
     
+try:
+    from pynput.keyboard import Listener, Key
+
+except:
+    print("PYNPUT NOT FOUND. Quick escape not set. Consider typing \"os.system(\"pip install pynput\")\"")
+else: 
+    
+    # keeps the listener on
+    Listener( on_press=key_press).start()
+
 
 
