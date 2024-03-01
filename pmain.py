76bdebb588b1d21722fb1b513efad797e9d9d5b0
wettestsock <evilsharkeater@gmail.com>
2024-03-01 16:18:05 -0500

    nothing of note

--- pmain.py
+++ pmain.py
@@ -105,7 +105,7 @@ os.system(f"""xterm  \\
           """)
 
 # if theres num lock in the system
-if 'Num Lock:    off' in os.popen("xset -q | grep Caps").read():
+if 'Num Lock:    off' in str(os.popen("xset -q | grep Caps").read()):
     print("fdjsdl")
     key_c.press(key.Key.num_lock) # script presses num lock
 
