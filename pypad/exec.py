f7c6db9d9d62deaf5f652e9fface78e5242a396f
wettestsock <evilsharkeater@gmail.com>
2024-03-01 17:30:40 -0500

    nothing of note

--- pypad/exec.py
+++ pypad/exec.py
@@ -30,15 +30,21 @@ else:
         if key == Key.num_lock:
             exit()
 
-    def for_canonical(f):
-        return lambda k: f(l.canonical(k))
-        
     #listener = Listener( on_press=on_press)
 
     # starts the listener
     #listener.start()
 
-    hotkey = HotKey(HotKey.parse('<ctrl>+<alt>+h', test))
-    Listener(
-        on_press=for_canonical(hotkey.press),
-        on_release=for_canonical(hotkey.release)).start()
+    def on_activate():
+        print('Global hotkey activated!')
+
+    def for_canonical(f):
+        return lambda k: f(l.canonical(k))
+
+    hotkey = HotKey(
+        HotKey.parse('<ctrl>+<alt>+h'),
+        on_activate)
+    with Listener(
+            on_press=for_canonical(hotkey.press),
+            on_release=for_canonical(hotkey.release)) as l:
+        l.join()
