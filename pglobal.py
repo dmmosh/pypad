32aa9ac13061e99a07160b62aa7e4a538f9d3bb9
wettestsock <evilsharkeater@gmail.com>
2024-03-01 16:42:43 -0500

    nothing of note

--- pglobal.py
+++ pglobal.py
@@ -31,7 +31,8 @@ def quit_all():
     r.destroy()
 
 # COMPUTER INFORMATION
-width, height = str(os.popen("(xrandr  | grep \* | cut -d' ' -f4) | head -n 1").read()).split('x') # gets width and height of the computer
+res = str(os.popen("(xrandr  | grep \* | cut -d' ' -f4) | head -n 1").read()).replace('\n', '').split('x')
+width, height = res[0], res[1] # gets width and height of the computer
 #colors = pd.read_excel(dir_loc+"/data.xlsx") #reads the excel sheet of colors
 #print(colors) # debug
 
