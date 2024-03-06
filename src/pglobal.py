9a5417e451387e6a2951c08ee8a9e677395f331f
wettestsock <evilsharkeater@gmail.com>
2024-03-06 00:55:43 -0500

    nothing of note

--- src/pglobal.py
+++ src/pglobal.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+'/../')
 
 from pypad._vendor.pynput import keyboard as key, mouse
 from tkinter import * 
