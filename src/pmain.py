fc5d249fd4f5fac4809a1a4f3065247d64e9f64e
wettestsock <evilsharkeater@gmail.com>
2024-03-10 18:17:51 -0400

    nothing of note

--- src/pmain.py
+++ src/pmain.py
@@ -28,8 +28,11 @@ i dont recommend running the program through python because of these issues
 
 '''
 
-# if theres another pypad instance running, 
-print(sys_out('pidof pypad').split(' '))
+# pypad compiled gives out 2 pids, which are tracked by pidof
+# if more than 1 instance, itll run 4, 6, etc
+# print(sys_out('pidof pypad').split(' '))
+if (len(sys_out('pidof pypad').split(' ')) > 2):
+    sys.exit()
 
 def settings_window(): # only imports the settings class if it's called
     import putils
