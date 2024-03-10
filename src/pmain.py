9b25de9934779dbd390a3eb677ffe3595a3ce3da
wettestsock <evilsharkeater@gmail.com>
2024-03-10 18:14:56 -0400

    nothing of note

--- src/pmain.py
+++ src/pmain.py
@@ -29,7 +29,7 @@ i dont recommend running the program through python because of these issues
 '''
 
 # if theres another pypad instance running, 
-print(sys_out('pidof pypad'))
+print(sys_out('pidof pypad').replace('\n', '').split(' '))
 
 def settings_window(): # only imports the settings class if it's called
     import putils
