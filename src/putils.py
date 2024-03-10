f30f0a1ab8f26722599d1089f4491ba3c3e6b53d
wettestsock <evilsharkeater@gmail.com>
2024-03-10 16:40:22 -0400

    nothing of note

--- src/putils.py
+++ src/putils.py
@@ -236,7 +236,7 @@ class settings:
                            font=gl.var['global_font'],
                            background=gl.var['color_fg'],
                            foreground=gl.var['color_bg'],
-                           width=60)
+                           width=5)
         self.width.config(validatecommand=((self.width.register(self.callback)), '%P'))
         self.width.insert(END, str(gl.var['font_size']))
         self.width.pack(anchor=SW)
